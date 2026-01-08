efec05032c7bbe69114f7a5fc72ee16366643cee
Melis KARAKAŞ <melis.karakas@faydam.com.tr>
2026-01-08 18:48:21 +0300

    [GEP-002] dersler sınıfı eklenmiştir.

---
+++ ORM.Models/Models/lessons.cs
@@ -0,0 +1,19 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using ORM.Shared;
+
+namespace ORM.Models.Models
+{
+    [Table(nameof(lessons))]
+    public class lessons:BaseModel
+    {
+        [ExplicitKey]
+        public Guid id { get; set; }
+        public string lesson_code { get; set; }
+    }
+}
