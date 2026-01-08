d75abe4ab94708440019b63353b02bb0443cc695
Melis KARAKAŞ <melis.karakas@faydam.com.tr>
2026-01-08 18:41:12 +0300

    öğrenciler sınıfı oluşturulmuştur.

--- ORM.Models/Models/students.cs
+++ ORM.Models/Models/students.cs
@@ -9,21 +9,18 @@ using ORM.Shared;
 
 namespace ORM.Models.Models
 {
-    [Table(nameof(users))]
-    public class users : BaseModel
+    [Table(nameof(students))]
+    public class students:BaseModel
     {
         [ExplicitKey]
         public Guid id { get; set; }
-        public string user_name { get; set; }
-        public string password { get; set; }
         public string name_surname { get; set; }
+        public string student_number { get; set; }
         public string email { get; set; }
-        public int user_type { get; set; }
         public string phone_number { get; set; }
         public DateTime last_login_time { get; set; }
         public bool is_registration { get; set; }
         public Guid created_by { get; set; }
         public Guid modified_by { get; set; }
-
     }
 }
